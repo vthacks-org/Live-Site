a9a1f08f00c4ad85f4a0f1012f2e36ede1a4be6e
Jacob Miller <jacobmiller22@vt.edu>
2021-01-26 16:35:30 -0500

    netlify server render workaround num 2

--- src/hooks/useUserLog.ts
+++ src/hooks/useUserLog.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react"
 
 function useUserLog(): [boolean, (val: boolean) => void] {
-  console.log("WINDOW IS: ", window)
-  if (window === undefined || window === null) {
+  if (typeof window === `undefined`) {
     return
   }
+
   const initialValue =
     window.localStorage.getItem("newcomer") === "true" ||
     window.localStorage.getItem("newcomer") === null
       ? true
       : false
-  console.log(initialValue)
+
   const [newcomer, setValue] = useState(initialValue)
   const setNewcomer = (val: boolean) => {
     if (val) {
